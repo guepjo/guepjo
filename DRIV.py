81fd3108d496ef9969f21d2e2b6eb604f0ec2dba
guepjo <62314105+guepjo@users.noreply.github.com>
2020-03-22 10:12:20 -0500

    Add files via upload

---
+++ DRIV.py
@@ -0,0 +1,12 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import scipy
+from scipy.io.wavfile import write
+
+
+fs = 44100  # Sample rate
+seconds = 110 # Duration of recording
+
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+sd.wait()  # Wait until recording is finished
+write('output.wav', fs, myrecording)  # Save as WAV file 
